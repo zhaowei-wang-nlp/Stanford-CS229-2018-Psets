6ae568aba5ab92c08acbdd69431f4b769a9e06c1
WANGZhaowei-Wesley <1303853063@qq.com>
2020-09-04 11:16:43 +0800

    finish the Pset2

---
+++ ps2/src/p01_lr.py
@@ -0,0 +1,73 @@
+# Important note: you do not have to modify this file for your homework.
+
+import util
+import numpy as np
+
+
+def calc_grad(X, Y, theta):
+    """Compute the gradient of the loss with respect to theta."""
+    m, n = X.shape
+
+    margins = Y * X.dot(theta)
+    probs = 1. / (1 + np.exp(margins))
+    grad = -(1./m) * (X.T.dot(probs * Y))
+
+    return grad
+
+
+def logistic_regression(X, Y):
+    """Train a logistic regression model."""
+    m, n = X.shape
+    theta = np.zeros(n)
+    learning_rate = 5
+
+    i = 0
+    while True:
+        i += 1
+        prev_theta = theta
+        grad = calc_grad(X, Y, theta)
+        theta = theta - learning_rate * grad
+        hx = 1/ (1 + np.exp(-X.dot(theta)))
+        if i % 10000 == 0:
+            print('Finished %d iterations' % i)
+            loss = -np.log(hx[Y == 1]).sum()
+            loss -= np.log(1 - hx[Y == -1]).sum()
+            pred_y = 2 * (hx >= 0.5) - 1
+            print(loss, theta, np.linalg.norm(prev_theta - theta))
+        if np.linalg.norm(prev_theta - theta) < 1e-15:
+            print('Converged in %d iterations' % i)
+            break
+    return
+
+
+def main():
+    print('==== Training model on data set A ====')
+    Xa, Ya = util.load_csv('../data/ds1_a.csv', add_intercept=True)
+    logistic_regression(Xa, Ya)
+
+    print('\n==== Training model on data set B ====')
+    Xb, Yb = util.load_csv('../data/ds1_b.csv', add_intercept=True)
+    logistic_regression(Xb, Yb)
+
+def plot(x, y, save_path, correction=1.0):
+    """Plot dataset and fitted logistic regression parameters.
+
+    Args:
+        x: Matrix of training examples, one per row.
+        y: Vector of labels in {0, 1}.
+        theta: Vector of parameters for logistic regression model.
+        save_path: Path to save the plot.
+        correction: Correction factor to apply (Problem 2(e) only).
+    """
+    # Plot dataset
+    plt.figure()
+    plt.plot(x[y == 1, -2], x[y == 1, -1], 'bx', linewidth=2)
+    plt.plot(x[y == -1, -2], x[y == -1, -1], 'go', linewidth=2)
+
+    # Add labels and save to disk
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.savefig(save_path)
+
+if __name__ == '__main__':
+    main()
